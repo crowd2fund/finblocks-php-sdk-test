7f2829777e59c381c0f431f6f7f68c10335e975b
David Garcia <david.garcia@deralia.com>
2018-12-21 15:17:13 +0000

    Add Author and Version annotations

--- src/Exception/FinBlocksException.php
+++ src/Exception/FinBlocksException.php
@@ -2,6 +2,11 @@
 
 namespace FinBlocks\Exception;
 
+/**
+ * @author    David Garcia <me@davidgarcia.cat>
+ * @copyright FinBlocks
+ * @version   1.0.0
+ */
 class FinBlocksException extends \RuntimeException
 {
 }
