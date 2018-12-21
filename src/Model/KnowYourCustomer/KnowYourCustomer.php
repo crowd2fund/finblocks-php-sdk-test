72cb4fc24160b1bf67ebcb4c887ceffcd9272319
David Garcia <david.garcia@deralia.com>
2018-12-21 15:30:09 +0000

    Add KYC's Model and Tests

---
+++ src/Model/KnowYourCustomer/KnowYourCustomer.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace FinBlocks\Model\KnowYourCustomer;
+
+use FinBlocks\Exception\FinBlocksException;
+use FinBlocks\Model\BaseModelInterface;
+
+/**
+ * @author    David Garcia <me@davidgarcia.cat>
+ * @copyright FinBlocks
+ *
+ * @version 1.0.0
+ * @since   1.0.0
+ */
+class KnowYourCustomer implements BaseModelInterface
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $documentId;
+
+    /**
+     * @var string
+     */
+    private $status;
+
+    /**
+     * @var string
+     */
+    private $refusedReason;
+
+    /**
+     * @var string|null
+     */
+    private $label;
+
+    /**
+     * @var string|null
+     */
+    private $tag;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime|null
+     */
+    private $processedAt;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $documentId
+     */
+    public function setDocumentId(string $documentId)
+    {
+        $this->documentId = $documentId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDocumentId(): string
+    {
+        return $this->documentId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRefusedReason(): string
+    {
+        return $this->refusedReason;
+    }
+
+    /**
+     * @param string|null $label
+     */
+    public function setLabel(string $label = null)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param string|null $tag
+     */
+    public function setTag(string $tag = null)
+    {
+        $this->tag = $tag;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getTag()
+    {
+        return $this->tag;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreatedAt(): \DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @return \DateTime|null
+     */
+    public function getProcessedAt()
+    {
+        return $this->processedAt;
+    }
+
+    /**
+     * Converts the Model to an array with all fields and format that we can use for the POST request.
+     *
+     * @return array
+     */
+    public function httpCreate(): array
+    {
+        return [
+            'documentId' => $this->documentId,
+            'label' => $this->label,
+            'tag' => $this->tag,
+        ];
+    }
+
+    /**
+     * Converts the Model to an array with all fields and format that we can use for the PUT request.
+     *
+     * @return array
+     */
+    public function httpUpdate(): array
+    {
+        throw new FinBlocksException('Impossible to update the KYC');
+    }
+}
---
+++ tests/Model/KnowYourCustomer/KnowYourCustomerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace FinBlocks\Tests\Model\KnowYourCustomer;
+
+use FinBlocks\Exception\FinBlocksException;
+use FinBlocks\Model\KnowYourCustomer\KnowYourCustomer;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author    David Garcia <me@davidgarcia.cat>
+ * @copyright FinBlocks
+ *
+ * @version 1.0.0
+ * @since   1.0.0
+ */
+class KnowYourCustomerTest extends TestCase
+{
+    public function testSettersForCardModel()
+    {
+        $model = new KnowYourCustomer();
+        $model->setDocumentId('documentId');
+        $model->setLabel('label');
+        $model->setTag('tag');
+
+        $this->assertEquals('documentId', $model->getDocumentId());
+        $this->assertEquals('label', $model->getLabel());
+        $this->assertEquals('tag', $model->getTag());
+    }
+
+    public function testCreateArray()
+    {
+        $model = new KnowYourCustomer();
+
+        $array = $model->httpCreate();
+
+        $this->assertCount(3, $array);
+
+        $this->assertArrayHasKey('documentId', $array);
+        $this->assertArrayHasKey('label', $array);
+        $this->assertArrayHasKey('tag', $array);
+    }
+
+    public function testUpdateArray()
+    {
+        $this->expectException(FinBlocksException::class);
+
+        $model = new KnowYourCustomer();
+        $model->httpUpdate();
+    }
+}
