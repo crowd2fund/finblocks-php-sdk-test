af75efa65e5785b81c27e409ceb7f56af5ed58cf
David Garcia <david.garcia@deralia.com>
2018-12-21 12:13:55 +0000

    Add BaseModel interface

---
+++ src/Model/BaseModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FinBlocks\Model;
+
+/**
+ * @author    David Garcia <me@davidgarcia.cat>
+ * @copyright FinBlocks
+ * @version   1.0.0
+ */
+interface BaseModel
+{
+    /**
+     * Converts the Model to an array with all fields and format that we can use for the POST request.
+     *
+     * @return array
+     */
+    public function httpCreate(): array;
+
+    /**
+     * Converts the Model to an array with all fields and format that we can use for the PUT request.
+     *
+     * @return array
+     */
+    public function httpUpdate(): array;
+}
