5a48bb53b57ba89709ef6a2a988fef54b7a084f8
David Garcia <david.garcia@deralia.com>
2019-01-02 12:45:18 +0000

    Build KYC's model through JSON payload

--- src/Model/KnowYourCustomer/KnowYourCustomer.php
+++ src/Model/KnowYourCustomer/KnowYourCustomer.php
@@ -61,6 +61,28 @@ class KnowYourCustomer implements BaseModelInterface
      */
     private function __construct(string $jsonData = null)
     {
+        if (!empty($jsonData)) {
+            try {
+                $arrayData = json_decode($jsonData, true);
+
+                if (JSON_ERROR_NONE !== json_last_error()) {
+                    throw new \InvalidArgumentException(json_last_error_msg(), json_last_error());
+                }
+
+                foreach ($arrayData as $property => $content) {
+                    switch ($property) {
+                        case 'createdAt':
+                        case 'processedAt':
+                            $this->$property = !empty($content) ? new \DateTime($content) : $content;
+                            break;
+                        default:
+                            $this->$property = $content;
+                    }
+                }
+            } catch (\Throwable $throwable) {
+                throw new FinBlocksException($throwable->getMessage(), $throwable->getCode(), $throwable);
+            }
+        }
     }
 
     /**
--- tests/Model/KnowYourCustomer/KnowYourCustomerTest.php
+++ tests/Model/KnowYourCustomer/KnowYourCustomerTest.php
@@ -27,6 +27,40 @@ class KnowYourCustomerTest extends TestCase
         $this->assertEquals('tag', $model->getTag());
     }
 
+    public function testCreateFilledModelFromJsonPayload()
+    {
+        $model = KnowYourCustomer::createFromPayload('{
+  "id": "1111",
+  "documentId": "2222",
+  "status": "refused",
+  "refusedReason": "Expired document",
+  "label": "Document\'s Label",
+  "tag": "Document\'s Tag",
+  "createdAt": "2019-01-02T12:40:21.919Z",
+  "processedAt": "2019-01-02T12:40:21.919Z"
+}');
+
+        $this->assertEquals('1111', $model->getId());
+        $this->assertEquals('2222', $model->getDocumentId());
+        $this->assertEquals('refused', $model->getStatus());
+        $this->assertEquals('Expired document', $model->getRefusedReason());
+        $this->assertEquals('Document\'s Label', $model->getLabel());
+        $this->assertEquals('Document\'s Tag', $model->getTag());
+
+        $this->assertInstanceOf(\DateTime::class, $model->getCreatedAt());
+        $this->assertInstanceOf(\DateTime::class, $model->getProcessedAt());
+
+        $this->assertEquals('2019-01-02 12:40:21', $model->getCreatedAt()->format('Y-m-d H:i:s'));
+        $this->assertEquals('2019-01-02 12:40:21', $model->getProcessedAt()->format('Y-m-d H:i:s'));
+    }
+
+    public function testCreateFilledModelFromWrongJsonPayload()
+    {
+        $this->expectException(FinBlocksException::class);
+
+        KnowYourCustomer::createFromPayload('This is not a JSON payload');
+    }
+
     public function testCreateArray()
     {
         $model = KnowYourCustomer::create();
