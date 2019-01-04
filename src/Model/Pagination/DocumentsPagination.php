ffcda337abff80faeeb47e58352a50a94cff4c40
David Garcia <david.garcia@deralia.com>
2019-01-04 10:20:02 +0000

    Build Document's Pagination model through JSON payload

--- src/Model/Pagination/DocumentsPagination.php
+++ src/Model/Pagination/DocumentsPagination.php
@@ -2,7 +2,10 @@
 
 namespace FinBlocks\Model\Pagination;
 
+use FinBlocks\Exception\FinBlocksException;
 use FinBlocks\Model\Document\AbstractDocument;
+use FinBlocks\Model\Document\DocumentIdCard;
+use FinBlocks\Model\Document\DocumentPassport;
 
 /**
  * @author    David Garcia <me@davidgarcia.cat>
@@ -34,7 +37,34 @@ final class DocumentsPagination extends AbstractPagination
      */
     public static function createFromPayload(string $jsonData)
     {
-        return new self($jsonData);
+        try {
+            $model = new self($jsonData);
+
+            $array = json_decode($jsonData, true);
+
+            foreach ($array['_embedded'] as $arrayModel) {
+                if (!array_key_exists('type', $arrayModel)) {
+                    throw new \RuntimeException('Unable to retrieve the type of document');
+                }
+
+                switch ($arrayModel['type']) {
+                    case DocumentIdCard::TYPE:
+                        $itemModel = DocumentIdCard::createFromPayload(json_encode($arrayModel));
+                        break;
+                    case DocumentPassport::TYPE:
+                        $itemModel = DocumentPassport::createFromPayload(json_encode($arrayModel));
+                        break;
+                    default:
+                        throw new \RuntimeException('Invalid document\'s type');
+                }
+
+                array_push($model->_embedded, $itemModel);
+            }
+
+            return $model;
+        } catch (\Throwable $throwable) {
+            throw new FinBlocksException($throwable->getMessage(), $throwable->getCode(), $throwable);
+        }
     }
 
     /**
