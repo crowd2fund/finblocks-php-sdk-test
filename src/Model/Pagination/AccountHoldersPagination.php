be03cef44d843297aea74066731047f36f6ee427
David Garcia <david.garcia@deralia.com>
2019-01-04 12:15:34 +0000

    Build Account Holder's Pagination through JSON payload

--- src/Model/Pagination/AccountHoldersPagination.php
+++ src/Model/Pagination/AccountHoldersPagination.php
@@ -2,7 +2,10 @@
 
 namespace FinBlocks\Model\Pagination;
 
+use FinBlocks\Exception\FinBlocksException;
 use FinBlocks\Model\AccountHolder\AbstractAccountHolder;
+use FinBlocks\Model\AccountHolder\AccountHolderBusiness;
+use FinBlocks\Model\AccountHolder\AccountHolderIndividual;
 
 /**
  * @author    David Garcia <me@davidgarcia.cat>
@@ -34,7 +37,34 @@ final class AccountHoldersPagination extends AbstractPagination
      */
     public static function createFromPayload(string $jsonData)
     {
-        return new self($jsonData);
+        try {
+            $model = new self($jsonData);
+
+            $array = json_decode($jsonData, true);
+
+            foreach ($array['_embedded'] as $arrayModel) {
+                if (!array_key_exists('type', $arrayModel)) {
+                    throw new \RuntimeException('Unable to retrieve the type of account holder');
+                }
+
+                switch ($arrayModel['type']) {
+                    case AccountHolderIndividual::TYPE:
+                        $itemModel = AccountHolderIndividual::createFromPayload(json_encode($arrayModel));
+                        break;
+                    case AccountHolderBusiness::TYPE:
+                        $itemModel = AccountHolderBusiness::createFromPayload(json_encode($arrayModel));
+                        break;
+                    default:
+                        throw new \RuntimeException('Invalid account holder\'s type');
+                }
+
+                array_push($model->_embedded, $itemModel);
+            }
+
+            return $model;
+        } catch (\Throwable $throwable) {
+            throw new FinBlocksException($throwable->getMessage(), $throwable->getCode(), $throwable);
+        }
     }
 
     /**
--- tests/Model/Pagination/AccountHoldersPaginationTest.php
+++ tests/Model/Pagination/AccountHoldersPaginationTest.php
@@ -34,4 +34,139 @@ class AccountHoldersPaginationTest extends TestCase
         $this->assertEquals(0, $model->getTotal());
         $this->assertEquals([], $model->getEmbedded());
     }
+
+    public function testCreateEmptyModelAccountHoldersPaginationFromPayload()
+    {
+        $model = Pagination\AccountHoldersPagination::createFromPayload('{
+            "total": 0,
+            "_links": {
+                "self": "string",
+                "first": "string",
+                "prev": "string",
+                "next": "string",
+                "last": "string"
+            },
+            "_embedded": [
+                {
+                    "id": "string",
+                    "type": "individual",
+                    "email": "string",
+                    "label": "string",
+                    "tag": "string",
+                    "givenName": "string",
+                    "middleName": "string",
+                    "familyName": "string",
+                    "dateOfBirth": "2019-01-04T12:10:07.926Z",
+                    "nationality": "string",
+                    "occupation": "string",
+                    "incomeRange": 1,
+                    "kyc": "restricted",
+                    "address": {
+                        "flatNumber": "string",
+                        "buildingNumber": "string",
+                        "buildingName": "string",
+                        "street": "string",
+                        "subStreet": "string",
+                        "town": "string",
+                        "state": "string",
+                        "postcode": "string",
+                        "country": "string"
+                    }
+                },
+                {
+                    "id": "string",
+                    "type": "business",
+                    "email": "string",
+                    "label": "string",
+                    "tag": "string",
+                    "givenName": "string",
+                    "middleName": "string",
+                    "familyName": "string",
+                    "dateOfBirth": "2019-01-04T12:10:07.926Z",
+                    "nationality": "string",
+                    "occupation": "string",
+                    "incomeRange": 1,
+                    "kyc": "restricted",
+                    "address": {
+                        "flatNumber": "string",
+                        "buildingNumber": "string",
+                        "buildingName": "string",
+                        "street": "string",
+                        "subStreet": "string",
+                        "town": "string",
+                        "state": "string",
+                        "postcode": "string",
+                        "country": "string"
+                    },
+                    "company": {
+                        "number": "string",
+                        "name": "string",
+                        "email": "string",
+                        "type": "business",
+                        "address": {
+                            "flatNumber": "string",
+                            "buildingNumber": "string",
+                            "buildingName": "string",
+                            "street": "string",
+                            "subStreet": "string",
+                            "town": "string",
+                            "state": "string",
+                            "postcode": "string",
+                            "country": "string"
+                        }
+                    }
+                }
+            ]
+        }');
+
+        $this->assertInstanceOf(Pagination\Links::class, $model->getLinks());
+
+        $this->assertCount(2, $model->getEmbedded());
+
+        $this->assertInstanceOf(Model\AccountHolder\AccountHolderIndividual::class, $model->getEmbedded()[0]);
+        $this->assertInstanceOf(Model\AccountHolder\AccountHolderBusiness::class, $model->getEmbedded()[1]);
+    }
+
+    public function testCreateModelAccountHoldersPaginationForUnknownTypeFromPayload()
+    {
+        $this->expectException(FinBlocksException::class);
+
+        Pagination\AccountHoldersPagination::createFromPayload('{
+            "total": 0,
+            "_links": {
+                "self": "string",
+                "first": "string",
+                "prev": "string",
+                "next": "string",
+                "last": "string"
+            },
+            "_embedded": [
+                {
+                    "id": "string"
+                }
+            ]
+        }');
+    }
+
+    public function testCreateModelAccountHoldersPaginationForInvalidTypeFromPayload()
+    {
+        $this->expectException(FinBlocksException::class);
+
+        Pagination\AccountHoldersPagination::createFromPayload('{
+            "total": 0,
+            "_links": {
+                "self": "string",
+                "first": "string",
+                "prev": "string",
+                "next": "string",
+                "last": "string"
+            },
+            "_embedded": [
+                {
+                    "id": "string",
+                    "type": "unknown"
+                }
+            ]
+        }');
+    }
 }
