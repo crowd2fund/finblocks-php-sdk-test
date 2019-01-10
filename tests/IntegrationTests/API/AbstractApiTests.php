2a7ab052ad1b9e41c8b5decbd44157437c1b5d45
David Garcia <david.garcia@deralia.com>
2019-01-10 12:59:21 +0000

    Don't mock the FinBlocks client, we use integration tests

--- tests/IntegrationTests/API/AbstractApiTests.php
+++ tests/IntegrationTests/API/AbstractApiTests.php
@@ -14,21 +14,10 @@ class AbstractApiTests extends TestCase
     protected $finBlocks;
 
     /**
-     * @var FinBlocks|MockObject
-     */
-    protected $finBlocksMock;
-
-    /**
      * {@inheritdoc}
      */
     protected function setUp()
     {
         $this->finBlocks = new FinBlocks('', '', '', true);
-
-        $finBlocksMock = $this->getMockBuilder(FinBlocks::class);
-        $finBlocksMock->setConstructorArgs(['key', 'cert', 'ca', true]);
-        $finBlocksMock->setMethods(['api', 'factories']);
-
-        $this->finBlocksMock = $finBlocksMock->getMock();
     }
 }
